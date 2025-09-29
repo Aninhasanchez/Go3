e6b9cc2f3eb792bb48153753837990e2af753ee8
Ana Luisa Sanchez <49624570809@>
2025-09-29 13:35:26 -0300

    29-09

---
+++ src/Paginas/Missao.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { missoes } from '../Dados/dadosMissao';
+import { MissaoCard } from '../Componentes/MissaoCard';
+import { MissaoModal } from '../Componentes/MissaoModal';
+
+export function Missao() {
+  const [missaoSelecionada, setMissaoSelecionada] = useState(null);
+
+
+  const concluirMissao = (id) => {
+    const inventario = JSON.parse(localStorage.getItem("Iventário")) || [];
+    const m = missoes.find((ms) => m.id === id);
+    
+  };
+
+  return (
+    <section className='conteiner'>
+      <h2>Missões</h2>
+      <div className="missoes-grid">
+        {missoes.map((m) => (
+          <MissaoCard
+            key={m.id} 
+            missao={m}  
+            onIniciarMissao={setMissaoSelecionada} 
+            concluida={missoesConcluidas.includes(m.id)} 
+          />
+        ))}
+      </div>
+
+      {missaoSelecionada && (
+        <MissaoModal 
+          missao={missaoSelecionada} 
+          onClose={() => setMissaoSelecionada(null)} 
+          onConcluir={() => concluirMissao(missaoSelecionada.id)} 
+        />
+      )}
+    </section>
+  );
+}
